4648a46c12f83ab62629bc84489984d6ef771aba
Tchiddo <27jsimms@gmail.com>
2025-09-11 14:59:55 -0400

    Update pennies.py

--- pennies.py
+++ pennies.py
@@ -21,26 +21,49 @@ post_nickles = int (post_dimes // nickles)
 post_pennies = int (post_nickles // pennies)
 
 # The amount of money left after converting from specific currency
-dollar_penny_deposit = int (penny_deposit - post_dollar * 100)
-quarter_penny_deposit = int (dollar_penny_deposit - post_quarters * 25)
-dime_penny_deposit = int (dollar_penny_deposit - post_dimes * 10)
-nickle_penny_deposit = int (dime_penny_deposit - post_nickles * 5)
-penny_penny_deposit = int (nickle_penny_deposit - post_pennies * 1)
+dollar_penny_deposit = int (penny_deposit - post_dollar * dollar_bill)
+quarter_penny_deposit = int (dollar_penny_deposit - post_quarters * quarters)
+dime_penny_deposit = int (dollar_penny_deposit - post_dimes * dimes)
+nickle_penny_deposit = int (dime_penny_deposit - post_nickles * nickles)
+penny_penny_deposit = int (nickle_penny_deposit - post_pennies * pennies)
+
+dollar_true = penny_deposit % dollar_bill == 0
+quarter_true =dollar_penny_deposit % quarters == 0
+dime_true = quarter_penny_deposit % dimes == 0
+nickle_true = dime_penny_deposit % nickles == 0
+penny_true = nickle_penny_deposit % pennies == 0
 
 print ("-------------------------------------------------")
 
 def penny_conversion():
-    if penny_deposit % dollar_bill == 0 :
-        print (post_dollar) 
+    if   dollar_true :
+        print (f"{post_dollar} dollars") 
+    
+    elif quarter_true :
+        print (f"{post_dollar} Dollars")
+        print (f"{dollar_penny_deposit // quarters} Quarters")
+    
+    elif dime_true :
+        print (f"{post_dollar} Dollars")
+        print (f"{dollar_penny_deposit // quarters} Quarters")
+        print (f"{quarter_penny_deposit // dimes} Dimes")
+    
+    elif nickle_true :
+        print (f"{post_dollar} Dollars")
+        print (f"{dollar_penny_deposit // quarters} Quarters")
+        print (f"{quarter_penny_deposit // dimes} Dimes")
+        print (f"{dime_penny_deposit // nickles} Nickles")
 
-    elif penny_deposit % dollar_bill != 0 :
-        print (post_dollar)
-        print (int(dollar_penny_deposit / quarters ))
+    elif penny_true :
+        print (f"{post_dollar} Dollars")
+        print (f"{dollar_penny_deposit // quarters} Quarters")
+        print (f"{quarter_penny_deposit // dimes} Dimes")
+        print (f"{dime_penny_deposit // nickles} Nickles")
+        print (f"{nickle_penny_deposit // pennies} Pennies")
+
+
     
 penny_conversion()
-
-
-
 
 
 
