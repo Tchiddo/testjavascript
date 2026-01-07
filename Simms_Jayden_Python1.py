90b4303e639a972252fee490b1e2e4373753f45d
Simms, Jayden <27jsimms@gmail.com>
2026-01-07 08:36:15 -0500

    Update Simms_Jayden_Python1.py

--- Simms_Jayden_Python1.py
+++ Simms_Jayden_Python1.py
@@ -32,13 +32,14 @@ class Laptop(Product):
         base_price = self.get_price()  # Refering to line 14
         return base_price * (
             1 - (discount / 100)
-        )  # Actually applying the discount part. Made up the numbers
+        )  # Actually applying the discount part.
 
     # Idk why it broke like that when I saved but I don't like it. Whatever.
 
 
 # As a User, I want to type in laptop details and see a formatted spec sheet. Because why would a user make it easy on me.
 # Wtf does it mean Import products module?
+
 name = input(
     "\nUser, I need a couple things if Im gonna make this spec sheet. Gimmie a name: "
 ).strip()
@@ -46,10 +47,19 @@ sku = input("\nAlright, now I need the SKU: ").strip()
 ram = input("\nThe RAM? ").strip()
 price = float(input("\nHow much does it cost? Gimmie the price: ").strip())
 storage = input("\nWhat't the storage? ").strip()
-touchscreen = input("\nAlright lastly, Is it touchscreen? ").strip()
+touchscreen = input("\nTouchscreen? (yes/no): ")
+
 user_laptop = Laptop(name, sku, price, ram, storage, touchscreen, 0)
-# Okay now that iv'e got a hold of the users I.P adress and mothers maiden name, we can finally instantiate
+# Okay now that iv'e got a hold of the users I.P adress and mothers maiden name, we can finally instantiate.
+
 print("\n--- SPEC SHEET ---")
 print(f"Device: {user_laptop.get_name()}")
+if touchscreen.lower() == "yes":
+    print("Is touchscreen")
+else:
+    pass
+
 print(f"Price: ${user_laptop.get_price()}")
-print(f"Discounted Price (15% off): ${user_laptop.calculate_discount_price(15):.2f}")
+print(
+    f"Discounted Price (15% off): ${user_laptop.calculate_discount_price(15):.2f}"
+)  # .2f in case it would come out as a hideous decimal
